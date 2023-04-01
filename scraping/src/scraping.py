26a651389262c0b32de23333d21eda441dc5a81c
Cristiano Deucher <1205053+cdeucher@users.noreply.github.com>
2023-04-01 11:59:40 -0300

    fix: debug craping image

--- scraping/src/scraping.py
+++ scraping/src/scraping.py
@@ -19,7 +19,27 @@ def get_html_page(url):
     HEADERS = ({
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
         'Accept-Language': 'en-US, en;q=0.5'})
-    return requests.get(url, headers=HEADERS).text
+    return requests.get(url, headers=HEADERS, timeout=(3.05, 27)).text
+
+def debug_html_page(url):
+    logger.info("get_html_page %s", url)
+    '''
+    from selenium import webdriver
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.common.by import By
+
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    options.binary_location='E:/dev/product-tracking/product-tracking-lambdas/scraping/src/geckodriver.exe'
+    browser = webdriver.Firefox(options=options)
+    browser.get(url)
+    wait = WebDriverWait(browser, 10)  # Wait for 10 seconds
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    page = browser.page_source
+    browser.quit()
+    return page
+    '''
 
 
 def scrape_html(webpage, url):
@@ -48,12 +68,17 @@ def kabum(dom):
     image = carousel.xpath('//div[@data-index="1"]//img/@src')
     logger.info("image - %s", len(image))
 
-    if len(price) == 0 or len(title) == 0 or len(image) == 0:
+    if len(price) == 0 or len(title) == 0: # or len(image) == 0:
         raise Exception(f"scrapping error: price, title, image:{len(price)},{len(title)},{len(image)}")
+
+    if len(image) == 0:
+        image = ""
+    else:
+        image = image[0]
 
     price = price[0].text.replace("R$", "").replace(".", "").replace(",", ".").replace(" ", "")
 
-    return price, title, image[0]
+    return price, title, image
 
 
 def amazon(dom):
@@ -85,7 +110,7 @@ def store_page(url):
 if __name__ == '__main__':
     try:
         url = "https://www.kabum.com.br/produto/181088/processador-amd-ryzen-5-5600g-3-9ghz-4-4ghz-max-turbo-cache-19mb-6-nucleos-12-threads-video-integrado-am4-100-100000252box"
-        store_page(url)
+        # store_page(url)
         price, title, image = scrap(url)
         logger.error("%s %s %s", price, title, image)
     except Exception as e:
