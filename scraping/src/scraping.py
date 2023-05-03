c234de8486fa218c4d55b980a55c9644c9a6ef76
Cristiano Deucher <1205053+cdeucher@users.noreply.github.com>
2023-05-03 19:50:03 -0300

    feat: add steam

--- scraping/src/scraping.py
+++ scraping/src/scraping.py
@@ -21,27 +21,6 @@ def get_html_page(url):
         'Accept-Language': 'en-US, en;q=0.5'})
     return requests.get(url, headers=HEADERS, timeout=(3.05, 27)).text
 
-def debug_html_page(url):
-    logger.info("get_html_page %s", url)
-    '''
-    from selenium import webdriver
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.common.by import By
-
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    options.binary_location='E:/dev/product-tracking/product-tracking-lambdas/scraping/src/geckodriver.exe'
-    browser = webdriver.Firefox(options=options)
-    browser.get(url)
-    wait = WebDriverWait(browser, 10)  # Wait for 10 seconds
-    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    page = browser.page_source
-    browser.quit()
-    return page
-    '''
-
-
 def scrape_html(webpage, url):
     logger.info("page %s", len(webpage))
     soup = BeautifulSoup(webpage, "html.parser")
@@ -52,6 +31,9 @@ def scrape_html(webpage, url):
         price, title, image = amazon(dom)
     elif "kabum" in url:
         price, title, image = kabum(dom)
+    elif "steampowered" in url:
+        price, title, image = steam(dom)
+
 
     return price, title, image
 
@@ -100,6 +82,20 @@ def amazon(dom):
 
     return price[0].text, title[0].text, image[0]
 
+def steam(dom):
+    title = dom.xpath('//*[@id="appHubAppName"]')
+    logger.info("title - %s", len(title))
+
+    price = dom.xpath('//*[@class="game_purchase_price price"]')
+    logger.info("price - %s", len(price))
+
+    image = dom.xpath('//*[@class="rightcol"]//img/@src')
+    logger.info("image - %s", len(image))
+
+    if len(price) == 0 or len(title) == 0 or len(image) == 0:
+        raise Exception(f"scrapping error: price, title, image:{len(price)},{len(title)},{len(image)}")
+
+    return price[0].text, title[0].text, image[0]
 
 def store_page(url):
     html = get_html_page(url)
@@ -109,8 +105,7 @@ def store_page(url):
 
 if __name__ == '__main__':
     try:
-        url = "https://www.kabum.com.br/produto/181088/processador-amd-ryzen-5-5600g-3-9ghz-4-4ghz-max-turbo-cache-19mb-6-nucleos-12-threads-video-integrado-am4-100-100000252box"
-        # store_page(url)
+        url = "https://store.steampowered.com/app/1062090/Timberborn/"
         price, title, image = scrap(url)
         logger.error("%s %s %s", price, title, image)
     except Exception as e:
